94ae1f19d8781a04e4a29ef077d0340463362ccf
HIUHIU <88829490+hieungo0503@users.noreply.github.com>
2024-05-04 20:19:11 +0700

    test commit

--- Core/Inc/ViettelSDK_Device_Management.h
+++ Core/Inc/ViettelSDK_Device_Management.h
@@ -30,7 +30,7 @@ static char static_null_string[1] =
 { 0 }; // intentionally left unused
 
 /* Extern variables */
-extern IWDG_HandleTypeDef hiwdg;
+//extern IWDG_HandleTypeDef hiwdg;
 extern TIM_HandleTypeDef htim2;
 
 struct ViettelSDK
