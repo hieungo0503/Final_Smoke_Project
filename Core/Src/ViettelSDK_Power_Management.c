1f39673bf6274c2a19453fac73fe812e6023e28b
HIUHIU <88829490+hieungo0503@users.noreply.github.com>
2024-05-08 00:09:58 +0700

    ok

--- Core/Src/ViettelSDK_Power_Management.c
+++ Core/Src/ViettelSDK_Power_Management.c
@@ -38,20 +38,21 @@ void setupSleepTimer(struct ViettelSDK *self)
 	writeLog(self, LOG_INFO, self->log_content, true);
 }
 
-void Enter_Stop1Mode(struct ViettelSDK *self, UART_HandleTypeDef *huart)
+void Enter_Stop1Mode(struct ViettelSDK *self, UART_HandleTypeDef *huart1, UART_HandleTypeDef *huart2)
 {
 	  self->StopMode = 1;
 
 	  HAL_SuspendTick();
 
-	  HAL_DisableDBGStopMode();
-	  //HAL_EnableDBGStopMode();
+	  //HAL_DisableDBGStopMode();
+	  HAL_EnableDBGStopMode();
 
       HAL_PWR_EnableSleepOnExit();
 	  /* enter STOP1 mode */
 	  HAL_PWREx_EnterSTOP1Mode(PWR_STOPENTRY_WFI);
 }
-void Exit_Stop1Mode()
+
+void Exit_Stop1Mode(struct ViettelSDK *self, UART_HandleTypeDef *huart1, UART_HandleTypeDef *huart2)
 {
 	SystemClock_Config();
 	HAL_ResumeTick();
